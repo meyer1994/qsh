703bbdc2313d4c5f513a594475c5f58471a9b8a1
Joao Meyer <1994meyer@gmail.com>
2024-11-12 22:19:02 -0300

    Fixed mypy

--- handler.py
+++ handler.py
@@ -1,14 +1,16 @@
 import json
 import logging
+from typing import Any
 
 from mangum import Mangum
+from mangum.types import LambdaContext
 
 from qsh.app import app
 
 logger = logging.getLogger(__name__)
 
 
-def handler(event, context):
+def handler(event: dict[str, Any], context: LambdaContext) -> dict[str, Any]:
     logger.info(json.dumps(event))
     handler = Mangum(app)
     return handler(event, context)
